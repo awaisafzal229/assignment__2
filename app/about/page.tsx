6802387e8560ddd232bc37033d94ff0c45bf0236
awaisafzal229 <129833448+awaisafzal229@users.noreply.github.com>
2023-11-24 17:45:33 +0500

    yes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -19,9 +19,9 @@ const hideMenu = () => {
     setIsMenuOpen(false);
 };
 
-// useEffect(()=>{
-//     showMenu(); // Call the function to show the menu on component mount
-// }, []);
+useEffect(()=>{
+    showMenu(); // Call the function to show the menu on component mount
+}, []);
   return (
     <>
 <section className="Sub-header">
@@ -51,7 +51,7 @@ const hideMenu = () => {
     <section className="about-us">
         <div className="row">
             <div className="about-col">
-                <h1>We are the world's largest University</h1>
+                <h1>We are the worlds largest University</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque in accusantium molestias maiores fuga vitae eos, ducimus officiis repudiandae voluptatem error. Laboriosam, numquam blanditiis aspernatur, nobis tempora consequatur id aliquam asperiores voluptatum iste animi voluptatem fugiat totam excepturi dolor.</p>
                 <a href="#" className="hero_btn btn">EXPLORE NOW</a>
             </div>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -19,9 +19,9 @@ const hideMenu = () => {
     setIsMenuOpen(false);
 };
 
-// useEffect(()=>{
-//     showMenu(); // Call the function to show the menu on component mount
-// }, []);
+useEffect(()=>{
+    showMenu(); // Call the function to show the menu on component mount
+}, []);
     return (
     <>
             <section className="Sub-header">
@@ -50,7 +50,6 @@ const hideMenu = () => {
   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111989.31313237658!2d77.32498792347556!3d28.699635117457326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cf1bb41c50fdf%3A0xe6f06fd26a7798ba!2sGhaziabad%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1630495771496!5m2!1sen!2sin"
   width={600} height={450}
   style={{border:0}}
-  allowFullScreen=""
   loading="lazy"
 ></iframe>
 
